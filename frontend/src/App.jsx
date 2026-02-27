6d0858fcdf14b62d374bd107e925d33d51b9aae8
HectorPartido <232685705+HectorPartido@users.noreply.github.com>
2026-02-27 14:02:03 -0500

    Build initial project structure - vite, tailwind, react router dom

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Planner from './pages/Planner'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
